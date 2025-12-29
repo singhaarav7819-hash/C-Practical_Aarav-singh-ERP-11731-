fa602f7a76b4e039f1a29932050a707bd0196c1e
Aarav Singh <aaravsingh@Aaravs-MacBook-Air.local>
2025-12-29 13:00:09 +0530

    update 3

--- question 10/q10.1.c
+++ question 10/q10.1.c
@@ -2,32 +2,42 @@
 #include <stdio.h>
 
 int main() {
-    // Aarav singh,Erp 11731//
-    int n, i, j;
-    int arr[10][10];
-    int sum = 0;
+    // Aarav singh,ERP 11731//
+    int a[10][10];
+    int r, c, i, j;
+    int max, min;
 
-    printf("Enter order of matrix: ");
-    scanf("%d", &n);
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
 
-    printf("Enter elements:\n");
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < n; j++) {
-            scanf("%d", &arr[i][j]);
+    printf("Enter elements of the 2D array:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
         }
     }
 
-    for(i = 0; i < n; i++) {
-        sum = sum + arr[i][i];
+    max = min = a[0][0];
+
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            if(a[i][j] > max)
+                max = a[i][j];
+            if(a[i][j] < min)
+                min = a[i][j];
+        }
     }
 
-    printf("Sum of diagonal elements = %d", sum);
+    printf("\nMaximum element = %d", max);
+    printf("\nMinimum element = %d", min);
 
     return 0;
 }
-/* 
-Enter order of matrix: 2 2
-Enter elements:
-1 2 3 4
-Sum of diagonal elements = 5 
+/*
+Enter number of rows and columns: 2 2
+Enter elements of the 2D array:
+1 2 3 4 
+
+Maximum element = 4
+Minimum element = 1
 */
